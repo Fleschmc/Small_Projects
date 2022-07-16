c08c558ef08b66a34853d92a1888664cc7ed7345
Michael Flesch <MichaelCFlesch@gmail.com>
2022-07-16 11:12:44 -0500

    Adding items to Rust Labs Scraping

--- Raiding/bot/scraping.py
+++ Raiding/bot/scraping.py
@@ -21,6 +21,12 @@ def run_scraping():
     construction_links = ['https://rustlabs.com' + soup.find('div', class_ = 'info-block group').find_all('a')[_]['href'] + '#tab=destroyed-by;filter=0,0,1,0,0,0;sort=4,0,2' for _ in range(len(soup.find('div', class_ = 'info-block group').find_all('a')))]
     
     time.sleep(3)
+
+    soup = BeautifulSoup(requests.get('https://rustlabs.com/group=items').content, 'html.parser')
+
+    item_links = ['https://rustlabs.com' + soup.find('div', class_ = 'info-block group').find_all('a')[_]['href'] + '#tab=destroyed-by;filter=0,0,1,0,0,0;sort=4,0,2' for _ in range(len(soup.find('div', class_ = 'info-block group').find_all('a')))]
+    
+    time.sleep(3)
     """
     Data Scraping
     """
@@ -28,55 +34,32 @@ def run_scraping():
     session = requests.Session()
 
     master = []
-    for link in build_links:
-        soup = BeautifulSoup(session.get(link, timeout = (12, 27)).content, 'html.parser')
-        
-        for explosive in soup.find_all('tr', {'data-group' : 'explosive'}):
-            data_dict = {}
-            data_dict['Object']   = soup.find('h1').text
-            data_dict['tool']     = explosive.find('td', class_ = 'left padding').text.strip().replace('Semi-Automatic Rifle', '').replace('(right click)', '').replace('Stuck', '').replace('Hunting Bow', '')
-            data_dict['quantity'] = int(explosive.find('td', class_ = 'no-padding').text.replace('~', '').replace(',', '').strip())
-            data_dict['time']     = explosive.find_all('td')[3].text
-            data_dict['type']     = 'Explosive'
-            data_dict['link']     = link
+    for link_group in [build_links, construction_links, item_links]:
 
-            master.append(data_dict)
+        for link in link_group:
+            soup = BeautifulSoup(session.get(link, timeout = (12, 27)).content, 'html.parser')
             
-        for explosive in soup.find_all('tr', {'data-group' : 'melee'}):
-            data_dict = {}
-            data_dict['Object']   = soup.find('h1').text
-            data_dict['tool']     = explosive.find('td', class_ = 'left padding').text.strip().replace('Semi-Automatic Rifle', '').replace('(right click)', '').replace('Stuck', '').replace('Hunting Bow', '')
-            data_dict['quantity'] = int(explosive.find('td', class_ = 'no-padding').text.replace('~', '').replace(',', '').strip())
-            data_dict['time']     = explosive.find_all('td')[3].text
-            data_dict['type']     = 'Melee'
-            data_dict['link']     = link
+            for explosive in soup.find_all('tr', {'data-group' : 'explosive'}):
+                data_dict = {}
+                data_dict['Object']   = soup.find('h1').text
+                data_dict['tool']     = explosive.find('td', class_ = 'left padding').text.strip().replace('Semi-Automatic Rifle', '').replace('(right click)', '').replace('Stuck', '').replace('Hunting Bow', '')
+                data_dict['quantity'] = int(explosive.find('td', class_ = 'no-padding').text.replace('~', '').replace(',', '').strip())
+                data_dict['time']     = explosive.find_all('td')[3].text
+                data_dict['type']     = 'Explosive'
+                data_dict['link']     = link
 
-            master.append(data_dict)
-            
-    for link in construction_links:
-        soup = BeautifulSoup(session.get(link, timeout = (12, 27)).content, 'html.parser')
-        
-        for explosive in soup.find_all('tr', {'data-group' : 'explosive'}):
-            data_dict = {}
-            data_dict['Object']   = soup.find('h1').text
-            data_dict['tool']     = explosive.find('td', class_ = 'left padding').text.strip().replace('Semi-Automatic Rifle', '').replace('(right click)', '').replace('Stuck', '').replace('Hunting Bow', '')
-            data_dict['quantity'] = int(explosive.find('td', class_ = 'no-padding').text.replace('~', '').replace(',', '').strip())
-            data_dict['time']     = explosive.find_all('td')[3].text
-            data_dict['type']     = 'Explosive'
-            data_dict['link']     = link
+                master.append(data_dict)
+                
+            for explosive in soup.find_all('tr', {'data-group' : 'melee'}):
+                data_dict = {}
+                data_dict['Object']   = soup.find('h1').text
+                data_dict['tool']     = explosive.find('td', class_ = 'left padding').text.strip().replace('Semi-Automatic Rifle', '').replace('(right click)', '').replace('Stuck', '').replace('Hunting Bow', '')
+                data_dict['quantity'] = int(explosive.find('td', class_ = 'no-padding').text.replace('~', '').replace(',', '').strip())
+                data_dict['time']     = explosive.find_all('td')[3].text
+                data_dict['type']     = 'Melee'
+                data_dict['link']     = link
 
-            master.append(data_dict)
-            
-        for explosive in soup.find_all('tr', {'data-group' : 'melee'}):
-            data_dict = {}
-            data_dict['Object']   = soup.find('h1').text
-            data_dict['tool']     = explosive.find('td', class_ = 'left padding').text.strip().replace('Semi-Automatic Rifle', '').replace('(right click)', '').replace('Stuck', '').replace('Hunting Bow', '')
-            data_dict['quantity'] = int(explosive.find('td', class_ = 'no-padding').text.replace('~', '').replace(',', '').strip())
-            data_dict['time']     = explosive.find_all('td')[3].text
-            data_dict['type']     = 'Melee'
-            data_dict['link']     = link
-
-            master.append(data_dict)
+                master.append(data_dict)
 
     return pd.DataFrame(master)
 
